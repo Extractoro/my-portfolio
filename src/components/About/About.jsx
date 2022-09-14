1fa7466768a0a55c8ccb473b7387a35f6fbb2fe0
Vadym Tytarenko (Extractoro) <67538058+Extractoro@users.noreply.github.com>
2022-09-14 17:31:38 +0300

    homepage

---
+++ src/components/About/About.jsx
@@ -0,0 +1,17 @@
+import Container from "../Container/Container";
+
+const About = () => {
+  return (
+    <Container>
+      <div>
+        <h5>My intro</h5>
+        <h2>About me</h2>
+      </div>
+      <div>
+        <div></div>
+      </div>
+    </Container>
+  );
+};
+//img
+export default About;
