b638f164255de572be7624fe79a942ecb4fb3835
Vadym Tytarenko (Extractoro) <67538058+Extractoro@users.noreply.github.com>
2022-09-14 17:51:57 +0300

    homepage

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,11 +4,18 @@ const About = () => {
   return (
     <Container>
       <div>
-        <h5>My intro</h5>
         <h2>About me</h2>
-      </div>
-      <div>
-        <div></div>
+
+        <div>
+          <div>
+            <ul>
+              <li></li>
+              <li></li>
+              <li></li>
+            </ul>
+          </div>
+          <div></div>
+        </div>
       </div>
     </Container>
   );
