21e1648e36a74370b9c49430a5f452ef2f35697e
Vadym Tytarenko (Extractoro) <67538058+Extractoro@users.noreply.github.com>
2022-09-14 18:04:36 +0300

    homepage

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,4 +1,6 @@
 import Container from "../Container/Container";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
 
 const About = () => {
   return (
@@ -8,11 +10,19 @@ const About = () => {
 
         <div>
           <div>
-            <ul>
-              <li></li>
-              <li></li>
-              <li></li>
-            </ul>
+            <Tabs>
+              <TabList>
+                <Tab>Title 1</Tab>
+                <Tab>Title 2</Tab>
+              </TabList>
+
+              <TabPanel>
+                <h2>Any content 1</h2>
+              </TabPanel>
+              <TabPanel>
+                <h2>Any content 2</h2>
+              </TabPanel>
+            </Tabs>
           </div>
           <div></div>
         </div>
